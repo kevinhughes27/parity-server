96b3be5d7529ce3749da82536c2fb92158e71739
Kevin Hughes <kevin.hughes@shopify.com>
2015-09-22 00:18:13 -0400

    break deploy function into smaller functions

--- bin/deploy_to_google.js
+++ bin/deploy_to_google.js
@@ -1,10 +1,17 @@
+/**
+ * This script allows you to do a partial deploy to a Google App Script project.
+ * It will update (overwrite) the specified file in the project but leave other
+ * files in tact.
+ */
+
 var fileId = '1IcYrOWSUW8pYM0cRLx_ZAkHnWw9rzXqcPyOkgMGoLrmlDVfjcsCfSUF4';
 var remoteFileName = 'Parser'
 var localFilePath = 'app/parser.js'
 
-var _ = require('underscore'),
+var _  = require('underscore'),
     fs = require('fs'),
     request = require('sync-request'),
+    google = require('googleapis'),
     PersitedGoogleAuth = require('./utils/persisted_google_auth');
 
 PersitedGoogleAuth.authenticate(
@@ -14,36 +21,9 @@ PersitedGoogleAuth.authenticate(
 
 function deploy(auth) {
   project = fetchProject(auth);
-
-  var service = google.drive('v2');
-
-
-  // find the parser file
-  parserFileIdx = _.findIndex(project.files, function(file){ return file.name == remoteFileName });
-
-  // overwrite the file source
-  project.files[parserFileIdx].source = fs.readFileSync(localFilePath).toString();
-
-  // save the file
-  var options = {
-    auth: auth,
-    fileId: '1IcYrOWSUW8pYM0cRLx_ZAkHnWw9rzXqcPyOkgMGoLrmlDVfjcsCfSUF4',
-    newRevision: true,
-    media: {
-      mimeType: 'application/vnd.google-apps.script+json',
-      body: JSON.stringify({ files: project.files })
-    }
-  };
-
-  service.files.update(options, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    } else {
-      console.log('Deploy successful.');
-      return;
-    }
-  });
+  project = updateProject(project);
+  saveProject(auth, project);
+  console.log('Deploy successful.');
 }
 
 /**
@@ -67,4 +47,38 @@ function fetchProject(auth) {
   }
 
   return project
+}
+
+/**
+ * Find the file to be replaced and then update its source to match the local file
+ */
+function updateProject(project) {
+  console.log('Updating project ...');
+  parserFileIdx = _.findIndex(project.files, function(file){ return file.name == remoteFileName });
+  project.files[parserFileIdx].source = fs.readFileSync(localFilePath).toString();
+  return project;
+}
+
+/**
+ * Save the project back to Google Drive
+ */
+function saveProject(auth, project) {
+  console.log('Saving project ...');
+  var service = google.drive('v2');
+  var options = {
+    auth: auth,
+    fileId: '1IcYrOWSUW8pYM0cRLx_ZAkHnWw9rzXqcPyOkgMGoLrmlDVfjcsCfSUF4',
+    newRevision: true,
+    media: {
+      mimeType: 'application/vnd.google-apps.script+json',
+      body: JSON.stringify({ files: project.files })
+    }
+  };
+
+  service.files.update(options, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+  });
 }
