b8bf4bb9bd909ca867f646b590737536e826aeb8
Kevin Hughes <kevinhughes27@gmail.com>
2016-10-02 14:18:37 -0400

    fix aync race in seed.js

--- seed.js
+++ seed.js
@@ -1,14 +1,28 @@
 import glob from 'glob';
+
+import LoopNext from 'loopnext';
+let syncLoop = new LoopNext().syncLoop;
+
 import child_process from 'child_process';
 let exec = child_process.exec;
 
 glob("test/files/*.json", (err, files) => {
-  files.forEach((file) => {
+  let iterations = files.length;
+
+  syncLoop(iterations, (l) => {
+    let idx = l.iteration();
+    let file = files[idx];
+
     console.log(`seeding with: ${file}`);
+
     let cmd = `curl -X POST\
            --data @${file}\
            -H "Content-Type: application/json"\
            http://localhost:3000/upload`;
-    exec(cmd);
+
+    exec(cmd, function(error, stdout, stderr) {
+      if(error) console.log(error);
+      l.next();
+    });
   });
 });
