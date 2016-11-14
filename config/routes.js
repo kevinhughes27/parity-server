2631170b2d0d26390d18b143996187184f493fd1
Kevin Hughes <kevinhughes27@gmail.com>
2016-11-14 17:48:29 -0500

    update routes to forward a wildcard to the client

--- config/routes.js
+++ config/routes.js
@@ -11,13 +11,17 @@ let clientPath = path.join(__dirname, '/../client/build')
 let docsPath = path.join(__dirname, '/../docs/')
 
 module.exports = function (app) {
-  app.use('/', express.static(clientPath))
-  app.use('/docs', express.static(docsPath))
-
   // API
   app.use('/', upload)
   app.use('/', teams)
   app.use('/', games)
   app.use('/', weeks)
   app.use('/', stats)
+
+  // Docs
+  app.use('/docs', express.static(docsPath))
+
+  // Client
+  app.use('/', express.static(clientPath))
+  app.use('/*', express.static(clientPath))
 }
