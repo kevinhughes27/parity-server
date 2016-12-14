088ec6470001a91db582f7218e9b9824a58db3dc
Kevin Hughes <kevinhughes27@gmail.com>
2016-12-13 23:55:43 -0500

    fix eslint issues in backup.js

--- bin/backup.js
+++ bin/backup.js
@@ -9,8 +9,8 @@ request.get(url, (error, response, body) => {
   let games = JSON.parse(body)
   games = _.filter(games, (g) => g.week === week)
 
-  games.forEach((game, idx,) => {
-    game = _.omit(game, ['stats' ,'_id'])
+  games.forEach((game, idx) => {
+    game = _.omit(game, ['stats', '_id'])
     let file = `test/files/week${week}_game${idx + 1}.json`
     fs.writeFileSync(file, JSON.stringify(game, null, 4))
   })
