f6685421707a64c6badf60283c2ea51d16157563
Kevin Hughes <kevinhughes27@gmail.com>
2017-02-13 15:50:43 -0500

    fix week num test

--- test/lib/calc_week_num_test.js
+++ test/lib/calc_week_num_test.js
@@ -5,13 +5,13 @@ import calcWeekNum from '../../lib/calc_week_num'
 
 describe('calcWeekNum', function () {
   it('returns week 1 for a date less than week 2', function () {
-    let date = new Date('2016-11-07')
+    let date = new Date('2017-02-19')
     let week = calcWeekNum(date)
     expect(week).to.equal(1)
   })
 
   it('returns week 2 for a date less than week 3 but greater than week 2', function () {
-    let date = new Date('2016-11-19')
+    let date = new Date('2017-02-26')
     let week = calcWeekNum(date)
     expect(week).to.equal(2)
   })
