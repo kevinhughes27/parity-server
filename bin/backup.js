4ca53773075bc1b04905bc9bb35c76c546710f0f
Kevin Hughes <kevinhughes27@gmail.com>
2017-02-21 12:54:12 -0500

    update backup script and pull down week 1 and 2

--- bin/backup.js
+++ bin/backup.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import fs from 'fs'
 import request from 'request'
+import zeroPad from 'zero-pad'
 
 let url = 'https://parity-server.herokuapp.com/games'
 let week = parseInt(process.argv[2])
@@ -11,7 +12,9 @@ request.get(url, (error, response, body) => {
 
   games.forEach((game, idx) => {
     game = _.omit(game, ['stats', '_id'])
-    let file = `db/week${week}_game${idx + 1}.json`
+    let weekNum = zeroPad(week)
+    let gameNum = idx + 1
+    let file = `db/week${weekNum}_game${gameNum}.json`
     fs.writeFileSync(file, JSON.stringify(game, null, 4))
   })
 })
