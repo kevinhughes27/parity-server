904fb0e741138d4cf855aca06596436859ea36c2
Kevin Hughes <kevinhughes27@gmail.com>
2017-10-21 15:40:17 -0400

    rewrite readme. Paste api docs and finish the empty api routes

--- app.py
+++ app.py
@@ -20,6 +20,31 @@ if (os.path.exists(db_path) == False):
 # Upload
 @app.route('/upload', methods=['POST'])
 def upload():
+    """
+    @api {post} /upload From Stats Keeper Client
+    @apiGroup Upload
+    @apiDescription This is only path which creates new data on the server.
+    The Stats Keeper Client uploads the stats to this endpoint and the server
+    does any required post-processing before saving the game to the database.
+     *
+    @apiParam {String} league the name of the league
+    @apiParam {Number} week the week number of the game
+    @apiParam {Object} teams a key for each team containing an array of player names
+    @apiParam {Array} event_string the array of events recored by the client
+     *
+    @apiParamExample {json} Example Upload:
+        {
+          "league": "ocua_16",
+          "week": 1,
+          "teams": {
+            "Karma Down Under": ["Alison Ward"],
+            "Kindha's Ongoing Disappointments": ["Jen Cluthe"]
+          },
+          "event_string": [
+            "Pull,Al Colantonio"
+          ]
+        }
+    """
     game = Game()
     game.league = request.form['league']
     game.week = calculate_week()
@@ -31,12 +56,116 @@ def upload():
     db.session.commit()
 
 # API
+@app.route('/teams')
+def teams():
+    """
+    @api {get} /teams List
+    @apiGroup Teams
+    @apiDescription Scrapes the latest team and roster data from Zuluru
+     *
+    @apiSuccess (200) {Object} teams key: team value: array of players
+     *
+    @apiSuccessExample {json} Example Response:
+       {
+         "Kindha's Ongoing Disappointments": {
+           "players": ["Kevin Hughes", "Jen Cluthe"],
+           "malePlayers": ["Kevin Hughes"],
+           "femalePlayers": ["Jen Cluthe"],
+         },
+         "Katie Parity": {
+           "players": ["Dan Thomson", "Andrea Proulx"],
+           "malePlayers": ["Dan Thomson"],
+           "femalePlayers": ["Andrea Proulx"],
+         }
+       }
+    """
+    return jsonify({})
+
+@app.route('/stats')
+def stats():
+    """
+    @api {get} /stats Cumulative
+    @apiGroup Weeks
+    @apiDescription Returns the summed stats for all weeks keeping the latest salary.
+    This response is calculated by summing all the games.
+    @apiSuccess (200) {Object} stats returns the summed stats for all weeks
+    @apiSuccessExample {json} Example Response:
+       {
+         "stats": {
+           "Al Colantonio": {"Pulls": 2, "SalaryDelta": 2000, "Salary": 50000}
+         }
+       }
+    """
+    return jsonify({})
+
 @app.route('/weeks')
 def weeks():
-    return jsonify([1,2,3])
+    """
+    @api {get} /weeks List
+    @apiGroup Weeks
+    @apiDescription Returns an array of week numbers. This is calculated by
+    a distinct query on game.week
+    @apiSuccess (200) {Array} weeks returns an array of week numbers
+    @apiSuccessExample {json} Example Response:
+       [
+         1,
+         2,
+         3
+       ]
+    """
+    return jsonify([1,2,3,4])
 
 @app.route('/weeks/<num>')
 def week(num):
+    """
+    @api {get} /weeks/:week Get
+    @apiGroup Weeks
+    @apiDescription Returns all the stats for the given week. The week is
+    calculated by merging all the games from that week.
+    @apiSuccess (200) {Object} week returns a week
+    @apiSuccessExample {json} Example Response:
+       {
+         "week": 1,
+         "stats": {
+           "Al Colantonio": {"Pulls": 1, "SalaryDelta": 2000, "Salary": 50000}
+         }
+       }
+     """
+    return jsonify({})
+
+@app.route('/games')
+def games():
+    """
+    @api {get} /games List
+    @apiGroup Games
+    @apiDescription Returns an array of all the games
+    @apiSuccess (200) {Array} games returns an array of games
+    """
+    return jsonify([1,2,3])
+
+@app.route('/games/<num>')
+def game(num):
+    """
+    @api {get} /games/:id Get
+    @apiGroup Games
+    @apiDescription Returns the data for a single game
+    @apiSuccess (200) {Object} game returns a game
+    @apiSuccessExample {json} Example Response:
+       {
+         "_id": "580bd3896df3906e619cd9f5",
+         "week": 1,
+         "teams": [
+           "Karma Down Under": ["Alison Ward"],
+           "Katie Parity": ["Dan Thomson"]
+         ],
+         "events": [
+           "Pull,Al Colantonio"
+         ],
+         "stats": {
+           "Al Colantonio": {"Pulls": 1, "SalaryDelta": 2000, "Salary": 50000}
+         }
+       }
+    """
     return jsonify({})
 
 # Client
