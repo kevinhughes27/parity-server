0a524bb9b4489576ead90d968eeea5d61dda4fae
Sebastien Belanger <sebastien.belanger@gmail.com>
2017-10-22 12:14:39 -0400

    new player model

--- models/player.py
+++ models/player.py
@@ -5,4 +5,9 @@ class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     rank = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "name": json.loads(self.teams),
+            "rank": json.loads(self.stats),
         }
