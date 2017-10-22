de3743b5928c80961633f0b7ff1ae5fe696c3c53
Sebastien Belanger <sebastien.belanger@gmail.com>
2017-10-22 12:49:44 -0400

    Player model without serialization

--- models/player.py
+++ models/player.py
@@ -5,9 +5,4 @@ class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     rank = db.Column(db.Integer)
-
-    def to_dict(self):
-        return {
-            "name": json.loads(self.teams),
-            "rank": json.loads(self.stats),
         }
