161b6182573bb6774e40322aca58a71623040e42
Kevin Hughes <kevinhughes27@gmail.com>
2017-10-22 14:32:18 -0400

    cleanup player model

--- models/player.py
+++ models/player.py
@@ -1,8 +1,6 @@
 from models.db import db
-import json
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     rank = db.Column(db.Integer)
-        }
