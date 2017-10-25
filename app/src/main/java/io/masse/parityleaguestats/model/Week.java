aa62e6fb76ed153226037cfb7a79b1e6588ce08f
Kevin Hughes <kevinhughes27@gmail.com>
2017-10-25 17:27:26 -0400

    finish week implementation and add tests

--- app/src/main/java/io/masse/parityleaguestats/model/Week.java
+++ app/src/main/java/io/masse/parityleaguestats/model/Week.java
@@ -6,14 +6,32 @@ import java.util.Date;
 public class Week {
     public final static int current() {
         Date now = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-        String[] weeks = new String[]{"2017/10/06", "2017/10/13", "2017/10/20"};
+        return current(now);
+    }
+
+    public final static int current(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyy/MM/dd");
+        String[] weeks = new String[]{
+                "2017/11/06",
+                "2017/11/13",
+                "2017/11/20",
+                "2017/11/27",
+                "2017/12/4",
+                "2017/12/11",
+                "2017/12/18",
+                "2018/01/8",
+                "2018/01/15",
+                "2018/01/22",
+                "2018/01/29",
+                "2018/02/5",
+        };
 
         int week = 0;
 
         try {
             for(String dateStr: weeks) {
-                if (now.after(sdf.parse(dateStr))) {
+                Date gameDay = sdf.parse(dateStr);
+                if (date.equals(gameDay) || date.after(gameDay)) {
                     week++;
                 }
             }
