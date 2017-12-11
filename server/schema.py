ed6311f8442a0a9829adb8ff12fb6833b0b4d511
Kevin Hughes <kevinhughes27@gmail.com>
2017-12-11 17:45:54 -0500

    wip uploadGame mutation

--- server/schema.py
+++ server/schema.py
@@ -6,6 +6,8 @@ from models import Player as PlayerModel
 from models import Team as TeamModel
 from models import Game as GameModel
 
+from upload import StatsCalculator
+
 
 class Player(SQLAlchemyObjectType):
 
@@ -26,6 +28,50 @@ class Game(SQLAlchemyObjectType):
     class Meta:
         model = GameModel
         interfaces = (relay.Node, )
+
+
+class uploadGame(graphene.Mutation):
+    class Arguments:
+        league = graphene.String()
+        week = graphene.String()
+
+        home_team = graphene.String()
+        home_score = graphene.String()
+        home_roster = graphene.types.json.JSONString()
+
+        away_team = graphene.String()
+        away_score = graphene.String()
+        away_roster = graphene.types.json.JSONString()
+
+        points = graphene.types.json.JSONString()
+
+    ok = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, **args):
+        game = Game(
+            league=args['league'],
+            week=args['week'],
+            home_team=args['homeTeam'],
+            home_score=args['homeScore'],
+            home_roster=args['homeRoster'],
+            away_team=args['awayTeam'],
+            away_score=args['awayScore'],
+            away_roster=args['awayRoster'],
+            points=args['points'],
+        )
+
+        db.session.add(game)
+        db.session.commit()
+
+        # calculate and save stats
+        stats = StatsCalculator(game.id, points).run()
+        for stat in stats:
+            db.session.add(stat[1])
+        db.session.commit()
+
+        ok = True
+        return uploadGame(ok=ok)
 
 
 class Query(graphene.ObjectType):
@@ -58,5 +104,8 @@ class Query(graphene.ObjectType):
         return GameModel.query.get(id)
 
 
+class Mutations(graphene.ObjectType):
+    uploadGame = uploadGame.Field()
 
-schema = graphene.Schema(query=Query, types=[Player, Team, Game])
+
+schema = graphene.Schema(query=Query, mutation=Mutations, types=[Player, Team, Game])
