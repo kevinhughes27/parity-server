f07266b79174c8c41f3a61599cb14dcef5983735
Kevin Hughes <kevinhughes27@gmail.com>
2017-12-11 17:25:37 -0500

    add some resolvers for finding single objects

--- server/schema.py
+++ server/schema.py
@@ -5,7 +5,6 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 from models import Player as PlayerModel
 from models import Team as TeamModel
 from models import Game as GameModel
-from models import Stats as StatsModel
 
 
 class Player(SQLAlchemyObjectType):
@@ -29,19 +28,35 @@ class Game(SQLAlchemyObjectType):
         interfaces = (relay.Node, )
 
 
-class Stats(SQLAlchemyObjectType):
-
-    class Meta:
-        model = StatsModel
-        interfaces = (relay.Node, )
-
-
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
+
+    player = SQLAlchemyConnectionField(Player)
     all_players = SQLAlchemyConnectionField(Player)
+    find_player = graphene.Field(lambda: Player, name = graphene.String())
+
+    def resolve_find_player(self, info, **args):
+        name = args['name']
+        return PlayerModel.query.filter_by(name=name).first()
+
+
+    team = SQLAlchemyConnectionField(Team)
     all_teams = SQLAlchemyConnectionField(Team)
+    find_team = graphene.Field(lambda: Team, name = graphene.String())
+
+    def resolve_find_team(self, info, **args):
+        name = args['name']
+        return TeamModel.query.filter_by(name=name).first()
+
+
+    game = SQLAlchemyConnectionField(Game)
     all_games = SQLAlchemyConnectionField(Game)
-    all_stats = SQLAlchemyConnectionField(Stats)
+    find_game = graphene.Field(lambda: Game, id = graphene.Int())
+
+    def resolve_find_game(self, info, **args):
+        id = args['id']
+        return GameModel.query.get(id)
 
 
-schema = graphene.Schema(query=Query, types=[Player, Team, Game, Stats])
+
+schema = graphene.Schema(query=Query, types=[Player, Team, Game])
