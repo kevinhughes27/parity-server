eca453ab9a6d5c8e581383ba2dccf921d4c2d7bc
Kevin Hughes <kevinhughes27@gmail.com>
2019-10-26 20:39:02 -0400

    seed now adds teams as required.
    * seed is now order dependent again otherwise it would overwrite team names with older names
    * added a json columm helper

--- server/lib/stats_calculator.py
+++ server/lib/stats_calculator.py
@@ -99,18 +99,27 @@ class StatsCalculator:
 from models import db, Stats, Player, Team
 
 class StatsCalculator:
-    def __init__(self, game, points):
+    def __init__(self, game):
         self.game = game
-        self.points = points
+        self.points = game.points
 
+        self.teams = Team.query.all()
+        self.players = Player.query.all()
+
+        self.verbose = False
         self.stats = {}
 
 
     def run(self):
-        for point in self.points:
+        for idx, point in enumerate(self.points):
+            if self.verbose:
+                print(f"Processing point {idx}")
             self.process_point(point)
 
-        return self.stats.items()
+        for name, player_stats in self.stats.items():
+            db.session.add(player_stats)
+
+        db.session.commit()
 
 
     def process_point(self, point):
@@ -179,16 +188,26 @@ class StatsCalculator:
 
 
     def find_or_create_player(self, player_name):
-        player = Player.query.filter_by(name=player_name).first()
+        player = self.find_player(player_name)
+
+        team = self.find_or_create_team_for_player(player_name)
 
         if player:
-            return player
+            return self.update_player(team, player)
         else:
-            return self.create_player(player_name)
+            return self.create_player(team, player_name)
 
 
-    def create_player(self, player_name):
-        team = self.find_or_create_team_for_player(player_name)
+    def find_player(self, player_name):
+        for player in self.players:
+            if player.name == player_name:
+                return player
+
+
+    def create_player(self, team, player_name):
+        if self.verbose:
+            print(f"Creating player: '{player_name}'")
+
         team_id = team.id if team else None
 
         player = Player(team_id=team_id, name=player_name)
@@ -196,40 +215,66 @@ class StatsCalculator:
         db.session.add(player)
         db.session.commit()
 
+        self.players.append(player)
+
         return player
 
 
-    # this ends up creating too many teams because the uploaded data
-    # only has the name which sometimes changes over the season.
-    #
-    # we could update the data to upload like so:
-    #
-    # homeTeam: {
-    #   zuluru_id:
-    #   name:
-    # }
-    #
+    def update_player(self, team, player):
+        if self.verbose:
+            print(f"Updating player: '{player.name}'")
+
+        team_id = team.id if team else None
+        player.team_id = team_id
+        db.session.add(player)
+        return player
+
+
     def find_or_create_team_for_player(self, player_name):
         # subsitute
         if player_name.endswith("(S)"):
             return None
 
-        team_name = self.game.home_team
-
+        # get the team the player is on
+        game_team = self.game.home_team
         if player_name in self.game.away_roster:
-            team_name = self.game.away_team
+            game_team = self.game.away_team
 
-        team = Team.query.filter_by(name=team_name).first()
+        team = self.find_team(game_team['id'])
 
         if team:
-            return team
+            return self.update_team(team, game_team['name'], game_team['id'])
         else:
-            return self.create_team(team_name)
+            return self.create_team(game_team['name'], game_team['id'])
 
 
-    def create_team(self, team_name):
-        team = Team(name=team_name)
+    def find_team(self, zuluru_id):
+        for team in self.teams:
+            if team.zuluru_id == zuluru_id:
+                return team
+
+
+    def create_team(self, team_name, zuluru_id):
+        if self.verbose:
+            print(f"Creating team: '{team_name}'")
+
+        team = Team(name=team_name, zuluru_id=zuluru_id)
+
         db.session.add(team)
-        db.session.commit()
+        db.session.commit() # need an id from this result
+
+        self.teams.append(team)
+
+        return team
+
+
+    def update_team(self, team, team_name, zuluru_id):
+        if self.verbose:
+            print(f"Updating team: '{team_name}'")
+
+        team.name = team_name
+        team.zuluru_id = zuluru_id
+
+        db.session.add(team)
         return team
 >>>>>>> refactoring the stats calculator and auto-create teams on upload
