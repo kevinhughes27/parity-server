82aa22633b4858767a518787b59264164309ec8e
Kevin Hughes <kevinhughes27@gmail.com>
2019-12-14 16:34:07 -0500

    set client to local server. allow league 4 in the api

--- server/app.py
+++ server/app.py
@@ -112,7 +112,7 @@ def game(league_id, id):
 @app.route('/api/leagues')
 def leagues():
     # earlier leagues are not ready yet
-    league_ids = [6,7,8,9,10]
+    league_ids = [4,6,7,8,9,10]
     query = League.query.filter(League.id.in_(league_ids)).order_by(League.zuluru_id.desc())
     leagues = [league.to_dict() for league in query]
     return jsonify(leagues)
