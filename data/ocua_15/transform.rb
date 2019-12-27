20042ee310486d54a972ec7cac2fa458eae4aa6e
Kevin Hughes <kevinhughes27@gmail.com>
2019-12-27 00:01:44 -0500

    work on 2015 transform - extract rosters

--- data/ocua_15/transform.rb
+++ data/ocua_15/transform.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'set'
 require 'json'
 require 'byebug'
 
@@ -7,16 +8,10 @@ data_directory = 'data/ocua_15'
 
 def rewrite_event_string(game_string)
   points = []
-
   point_start_idx = 0
-  direction_left = nil
-  last_point_left = nil
 
   game_string.each_with_index do |e, idx|
-    if e[0] == "Pull"
-      last_point_left = (game_string[idx+1][1] == ">>>>>>")
-
-    elsif e[0] == "POINT"
+    if e[0] == "POINT"
       point_end_idx = idx
 
       players_start = point_end_idx + 1
@@ -33,17 +28,13 @@ def rewrite_event_string(game_string)
       point_string = game_string[point_start_idx..point_end_idx]
 
       points << {
-        "offensePlayers" => offensePlayers,
-        "defensePlayers" => defensePlayers,
+        "offensePlayers" => offensePlayers.compact,
+        "defensePlayers" => defensePlayers.compact,
         "event_string" => point_string
       }
 
-      # set idx and direction
+      # set point_start_idx
       point_start_idx = point_end_idx + 6 + 1
-      last_point_left = !direction_left
-
-    elsif e[0] == "Direction"
-      direction_left = (e[1] == ">>>>>>")
     end
   end
 
@@ -55,7 +46,7 @@ def rewrite_event_string(game_string)
   #
   # what will adding this break?
   #
-  ended_on_point = (point_start_idx + 1 == game_string.length)
+  ended_on_point = (point_start_idx + 1 >= game_string.length)
   if !ended_on_point
     point_string = game_string[point_start_idx..-1]
     points << {
@@ -65,7 +56,7 @@ def rewrite_event_string(game_string)
     }
   end
 
-  # the data is now grouped by point
+  # the points data is now grouped by point
   # and shaped like this:
   # {
   #    offensePlayers: [],
@@ -125,7 +116,70 @@ def rewrite_event_string(game_string)
   points
 end
 
-def build_game_json(week:, home_row:, away_row:, event_string:)
+def build_rosters(points)
+  homeRoster = Set.new
+  awayRoster = Set.new
+
+  # assume home always pulls. Swap later if required
+  homeRoster += points[0]["defensePlayers"].compact.to_set
+  awayRoster += points[0]["offensePlayers"].compact.to_set
+
+  scorer = points[0]["events"].last["firstActor"]
+  offenseScored = points[0]["offensePlayers"].include?(scorer)
+
+  if offenseScored
+    homeRoster += points[1]["offensePlayers"].compact.to_set
+    awayRoster += points[1]["defensePlayers"].compact.to_set
+  else
+    homeRoster += points[1]["defensePlayers"].compact.to_set
+    awayRoster += points[1]["offensePlayers"].compact.to_set
+  end
+
+  # rosters should be basically correct now
+  # fill in against the rest of the game
+  points.each do |point|
+    home_is_offsense = (homeRoster & point["offensePlayers"].to_set).size > 1
+
+    offensePlayers = point["offensePlayers"].compact.to_set
+    defensePlayers = point["defensePlayers"].compact.to_set
+
+    if home_is_offsense
+      homeRoster += offensePlayers
+      awayRoster += defensePlayers
+    else
+      homeRoster += defensePlayers
+      awayRoster += offensePlayers
+    end
+  end
+
+  # re-score the game to check if I got home and away right
+  homeScore = 0
+  awayScore = 0
+
+  points.each do |point|
+    next unless point["events"].last["type"] == "POINT"
+    scorer = point["events"].last["firstActor"]
+    home_scored = homeRoster.include?(scorer)
+    if home_scored
+      homeScore += 1
+    else
+      awayScore += 1
+    end
+  end
+
+  if (homeRoster & awayRoster).size > 0
+    raise "roster overlap"
+  end
+
+  return {
+    homeRoster: homeRoster.to_a,
+    homeScore: homeScore,
+    awayRoster: awayRoster.to_a,
+    awayScore: awayScore
+  }
+end
+
+def build_game_json(week:, game_num:, home_row:, away_row:, event_string:)
   homeTeam = home_row[0]
   homeScore = home_row[1]
 
@@ -136,21 +190,41 @@ def build_game_json(week:, home_row:, away_row:, event_string:)
 
   points = rewrite_event_string(event_string)
 
+  data = build_rosters(points)
+
+  score = "#{homeScore} - #{awayScore}"
+  calcScore = "#{data[:homeScore]} - #{data[:awayScore]}"
+  revScore = "#{data[:awayScore]} - #{data[:homeScore]}"
+
+  if score != calcScore
+    if score == revScore
+      tmpRoster = data[:homeRoster]
+      data[:homeRoster] = data[:awayRoster]
+      data[:awayRoster] = tmpRoster
+    else
+      # yay the score calc always matches!!
+      # except for 1 instance of bad data
+      raise "score mismatch" unless week == 4 && game_num == 1
+    end
+  end
+
   return {
     "league_id" => LEAGUE_ID,
     "week" => week,
     "homeTeam" => homeTeam,
     "homeScore" => homeScore,
-    "homeRoster" => [],
+    "homeRoster" => data[:homeRoster],
     "awayTeam" => awayTeam,
     "awayScore" => awayScore,
-    "awayRoster" => [],
+    "awayRoster" => data[:awayRoster],
     "points" => points,
   }
 end
 
 def transform(data_directory)
-  Dir.glob("#{data_directory}/*.csv").sort.each do |file|
+  csv_files = Dir.glob("#{data_directory}/*.csv")
+
+  csv_files.sort.each do |file|
     week = file.split("week").last.gsub(".csv", "").to_i
 
     data = File.read(file)
@@ -172,6 +246,7 @@ def transform(data_directory)
 
     game_starts = (0..team_row_nums.length / 2 - 1).to_a.map { |i| i*2 }
 
+    # Each Game in the CSV
     game_starts.each_with_index do |idx, j|
       home_row_idx = team_row_nums[idx]
       away_row_idx = team_row_nums[idx+1]
@@ -185,14 +260,16 @@ def transform(data_directory)
         game_string_end -= 1
       end
 
+      game_num = j + 1
+
       game = build_game_json(
         week: week,
+        game_num: game_num,
         home_row: csv[home_row_idx],
         away_row: csv[away_row_idx],
         event_string: csv[game_string_start..game_string_end]
       )
 
-      game_num = j + 1
       File.write("#{data_directory}/week#{week}_game#{game_num}.json", JSON.pretty_generate(game) + "\n")
     end
   end
