4afe57f4081fe82b6839d1fc0e6b36915acec955
Kevin Hughes <kevinhughes27@gmail.com>
2020-01-18 13:45:28 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/parity-server/precache-manifest.00f627c489a110947d56a2647f5cdb93.js"
+  "/parity-server/precache-manifest.08ba179ad832de2efc871cd2c2833afb.js"
 );
 
 self.addEventListener('message', (event) => {
