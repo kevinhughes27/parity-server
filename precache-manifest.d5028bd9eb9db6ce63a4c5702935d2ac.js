5a9863cfc9f6b10877231921a70b10cf2606a015
Kevin Hughes <kevinhughes27@gmail.com>
2020-01-18 13:18:16 -0500

    Updates

---
+++ precache-manifest.d5028bd9eb9db6ce63a4c5702935d2ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baf15c6428f548e9292713f214ea00f",
+    "url": "/parity_tournament/index.html"
+  },
+  {
+    "revision": "2a3039b9129c5f4ba36b",
+    "url": "/parity_tournament/static/css/main.c4d50f7d.chunk.css"
+  },
+  {
+    "revision": "71b90ec08432253e0b65",
+    "url": "/parity_tournament/static/js/2.e9cda072.chunk.js"
+  },
+  {
+    "revision": "7b9f6a07e31b641a7b2d2f62ecd673bf",
+    "url": "/parity_tournament/static/js/2.e9cda072.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a3039b9129c5f4ba36b",
+    "url": "/parity_tournament/static/js/main.85c0cd09.chunk.js"
+  },
+  {
+    "revision": "3c49d6916051321da9aa",
+    "url": "/parity_tournament/static/js/runtime-main.7756269a.js"
+  }
+]);
