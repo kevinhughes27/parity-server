67846f0dedd2baa5a7313431bc265f718a5c6ba1
Kevin Hughes <kevinhughes27@gmail.com>
2021-10-26 16:52:22 -0400

    cli current league and api current league are not the same

--- server/app.py
+++ server/app.py
@@ -30,7 +30,7 @@ db.init_app(app)
 # Current League
 @app.route('/current_league')
 def current_league():
-    league = {'league': {'id': 14, 'name': '2021 Session 3'}}
+    league = {'league': {'id': 15, 'name': '2021/2020 Session 1'}}
     return jsonify(league)
 
 
