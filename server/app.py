09fcea1428cbcc6c2ef702c92d84702f3bae8ca4
Kevin Hughes <kevinhughes27@gmail.com>
2022-03-07 10:57:19 -0500

    update current league endpoint

--- server/app.py
+++ server/app.py
@@ -30,7 +30,7 @@ db.init_app(app)
 # Current League
 @app.route('/current_league')
 def current_league():
-    league = {'league': {'id': 15, 'name': '2021/2022 Session 1', 'lineSize': 6}}
+    league = {'league': {'id': 16, 'name': '2021/2022 Session 2', 'lineSize': 6}}
     return jsonify(league)
 
 
