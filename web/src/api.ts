3f62765963a6a290bf10c0ee19ce6754040612be
Kevin Hughes <kevinhughes27@gmail.com>
2022-03-06 21:28:43 -0500

    add new league to frontend

--- web/src/api.ts
+++ web/src/api.ts
@@ -35,6 +35,7 @@ const cachedFetch = (url: string, _options = {}) => {
 //   return leagues.filter((l) => l.id > 1)
 // }
 const leagues = [
+  { id: 16, name: "2021/2022 Session 2"},
   { id: 15, name: "2021/2022 Session 1"},
   { id: 14, name: "2021 Session 3"},
   { id: 13, name: "2020/2021 Session 1" },
