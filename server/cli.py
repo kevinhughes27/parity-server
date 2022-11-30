e7763ea421e7b10076b7fd4fdf1aa21c50a18ee0
Kevin Hughes <kevinhughes27@gmail.com>
2022-11-30 16:55:43 -0500

    Revert "recalc stats"
    This reverts commit 39485c366bf92454bc39c9a308af0cb27faffba4.

--- server/cli.py
+++ server/cli.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 import click
+import glob
+import os
+import re
+import json
+import urllib.request
+import requests
+from collections import defaultdict
 
 from app import app, cache
-from models import db, League, Game, Stats
-from lib import StatsCalculator, ZuluruSync
+from models import db, League
+from lib import ZuluruSync
 
 current_league_zid = 906
 current_league_id = 17
@@ -40,21 +47,54 @@ def zuluru_sync_current():
 
 
 @cli.command()
-@click.option('--week')
-def recalc(week):
-    with app.app_context():
-        games = Game.query.filter_by(league_id=current_league_id, week=week).all()
-        game_ids = [game.id for game in games]
+@click.option('--week', default=0)
+def backup(week):
+    click.echo('Downloading games...')
 
-        print("Deleting old stats")
-        stats = Stats.query.filter(Game.id.in_(game_ids)).all()
-        for stat in stats:
-            db.session.delete(stat)
-        db.session.commit()
+    league_id = current_league_id
 
-        print("Re-calculating stats")
-        for game in games:
-            StatsCalculator(game).run()
+    src_url = f"https://parity-server.herokuapp.com/api/{league_id}/games"
+    target_dir = "./tmp"
+
+    game_counts = defaultdict(int)
+
+    with urllib.request.urlopen(src_url) as url:
+        games = json.loads(url.read().decode())
+
+    if week > 0:
+        games = [game for game in games if game['week'] == week]
+
+    for game in games:
+        game_url = src_url + '/' + str(game['id'])
+
+        with urllib.request.urlopen(game_url) as url:
+            game = json.loads(url.read().decode())
+
+        week = str(game["week"])
+        game_counts[week] += 1
+        game_num = str(game_counts[week])
+
+        del game['id']
+        del game['stats']
+
+        file_name = "week" + week + "_game" + game_num + ".json"
+        fo = open(os.path.join(target_dir, file_name), "w")
+        fo.write(json.dumps(game, indent=2, sort_keys=True))
+        fo.close()
+
+    click.echo('Done')
+
+
+@cli.command()
+@click.option('--file')
+def re_upload(file):
+    url = 'http://localhost:5000/submit_game'
+
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    r = requests.post(url, data=open(file, 'rb'), headers=headers)
+    print(file, r.status_code)
+
+    click.echo('Done')
 
 
 if __name__ == "__main__":
