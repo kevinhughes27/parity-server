f1dbd12bb00e23ed786abee460a89c2fcccd066c
Kevin Hughes <kevinhughes27@gmail.com>
2024-03-24 13:15:00 -0400

    skip e2e test. too flakey

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -5,11 +5,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from urllib.request import urlopen
+import pytest
 
 from models import db
 from .test_base import TestBase
 
 
+@pytest.mark.skip(reason="too flakey")
 class AppTests(TestBase, FlaskTest, LiveServerTestCase):
     def setUp(self):
         db.create_all()
