69192295c857460edef530bf35ebcb91d61947ec
Kevin Hughes <kevinhughes27@gmail.com>
2025-05-21 10:47:22 -0400

    flatten modules for now. separate db and api types. api is working

--- server/config.py
+++ server/config.py
@@ -2,6 +2,33 @@ import os
 
 CURRENT_LEAGUE_ID = 22
 
+STAT_VALUES = {
+    "v2": {
+        "goals": 10000,
+        "assists": 10000,
+        "second_assists": 8000,
+        "d_blocks": 8000,
+        "throw_aways": -5000,
+        "threw_drops": -1000,
+        "drops": -4000,
+        "completions": 500,
+        "catches": 500,
+        "o_points_for": 1000,
+        "d_points_for": 2000,
+    },
+    "v1": {
+        "goals": 10000,
+        "assists": 10000,
+        "second_assists": 8000,
+        "d_blocks": 8000,
+        "throw_aways": -5000,
+        "threw_drops": -2500,
+        "drops": -5000,
+        "completions": 1000,
+        "catches": 1000,
+    },
+}
+
 
 def database_path():
     base_dir = os.path.abspath(os.path.dirname(__file__))
