85321632d8ed966a8f13d0a26f5a7c89e82c8399
Kevin Hughes <kevinhughes27@gmail.com>
2025-05-26 17:07:59 -0400

    port to vite
    ref: https://adhithiravi.medium.com/migrating-from-create-react-app-to-vite-a-modern-approach-76148adb8983

---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {global: 'window'},
+  build: {
+    outDir: 'build', // CRA's default build output
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
