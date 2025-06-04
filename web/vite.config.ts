51b2c0598492477e3f324dee0bd8d4fe91577819
Kevin Hughes <kevinhughes27@gmail.com>
2025-06-04 13:13:40 -0400

    add vite proxy config for submit_game. separate from api proxy but the same.
    stats recording and submitting works now

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
+      '/submit_game': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
     },
   },
 });
