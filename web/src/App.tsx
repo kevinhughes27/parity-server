7cdd29ef84414a1f19d3b6a2663d2d1f3081f0b0
Kevin Hughes <kevinhughes27@gmail.com>
2025-06-23 21:31:17 -0400

    update routes. rm EditGame

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,8 +15,7 @@ import TeamDashboard from './views/TeamDashboard';
 import StatKeeper from './views/StatKeeper';
 import LocalGame from './views/StatKeeper/LocalGame';
 import ViewGame from './views/StatKeeper/ViewGame';
-import NewGame from './views/StatKeeper/NewGame'; // Renamed import from NewGameSetup
-import EditGame from './views/StatKeeper/EditGame'; // Renamed import from EditGameRosters
+import NewGame from './views/StatKeeper/NewGame';
 
 function App() {
   return (
@@ -29,13 +28,12 @@ function App() {
         <Route path="/leaderboards" element={<StatsPage component={Leaderboards} />} />
         <Route path="/compare_players" element={<StatsPage component={ComparePlayers} />} />
         <Route path="/team_dashboard" element={<SalaryPage component={TeamDashboard} />} />
+
         {/* StatKeeper Routes */}
         <Route path="/stat_keeper" element={<StatKeeper />} />
+        <Route path="/stat_keeper/new_game" element={<NewGame />} />
         <Route path="/stat_keeper/game/:localGameId" element={<LocalGame />} />
         <Route path="/stat_keeper/view_game/:localGameId" element={<ViewGame />} />
-        <Route path="/stat_keeper/new_game" element={<NewGame />} /> {/* Updated component */}
-        {/* Route for editing existing game rosters, uses the new EditGame component and updated path */}
-        <Route path="/stat_keeper/edit_game/:localGameId" element={<EditGame />} />
       </Routes>
     </BrowserRouter>
   );
